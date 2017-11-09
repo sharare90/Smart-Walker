ab244a2322bb0266ddd3fc1b4668cd8fe59bd8c4
sharare90 <sharare.zehtabian@gmail.com>
2017-11-09 17:01:03 -0500

    first version of interface change color of buttons

--- UserInterface.py
+++ UserInterface.py
@@ -1,8 +1,26 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.uix.label import Label
+from kivy.properties import ListProperty, ObjectProperty
+
 
 class SmartWalker(Widget):
-    pass
+    front_left_leg = ObjectProperty()
+    ellipse_color_fl = ListProperty([1, 0, 0, 1])
+    ellipse_color_fr = ListProperty([1, 0, 0, 1])
+    ellipse_color_rl = ListProperty([1, 0, 0, 1])
+    ellipse_color_rr = ListProperty([1, 0, 0, 1])
+    ellipse_color_gy = ListProperty([1, 0, 0, 1])
+
+    def change_color(self, leg):
+        if leg == self.front_left_leg:
+            self.ellipse_color_fl = 1, 1, 1, 1
+        if leg == self.front_right_leg:
+            self.ellipse_color_fr = 1, 1, 1, 1
+        if leg == self.rear_left_leg:
+            self.ellipse_color_rl = 1, 1, 1, 1
+        if leg == self.rear_right_leg:
+            self.ellipse_color_rr = 1, 1, 1, 1
 
 
 class SmartApp(App):
