9377051f05e1414b7c65549ad0e684dc499c73bf
sharare90 <sharare.zehtabian@gmail.com>
2017-11-16 14:39:29 -0500

    Clock Label Added

--- UserInterface.py
+++ UserInterface.py
@@ -1,16 +1,25 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
-from kivy.uix.label import Label
-from kivy.properties import ListProperty, ObjectProperty
+from kivy.clock import Clock
+import time
+from kivy.properties import ListProperty, ObjectProperty, StringProperty
 
 
 class SmartWalker(Widget):
     front_left_leg = ObjectProperty()
+    front_right_leg = ObjectProperty()
+    rear_left_leg = ObjectProperty()
+    rear_right_leg = ObjectProperty()
     ellipse_color_fl = ListProperty([1, 0, 0, 1])
     ellipse_color_fr = ListProperty([1, 0, 0, 1])
     ellipse_color_rl = ListProperty([1, 0, 0, 1])
     ellipse_color_rr = ListProperty([1, 0, 0, 1])
     ellipse_color_gy = ListProperty([1, 0, 0, 1])
+    timer = ObjectProperty()
+    thisTime = StringProperty("")
+
+    def update(self, *args):
+        self.thisTime = str(time.asctime())
 
     def change_color(self, leg):
         if leg == self.front_left_leg:
@@ -25,7 +34,9 @@ class SmartWalker(Widget):
 
 class SmartApp(App):
     def build(self):
-        return SmartWalker()
+        s = SmartWalker()
+        Clock.schedule_interval(s.update, 1)
+        return s
 
 
 if __name__ == '__main__':
