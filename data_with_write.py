6e7938622ed7afa038966bb61c593cb1c948f040
sharare90 <sharare.zehtabian@gmail.com>
2017-11-17 17:58:22 -0500

    print sensor value

--- data_with_write.py
+++ data_with_write.py
@@ -2,7 +2,7 @@ import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
 
-for i in range(40):
-    GPIO.setup(i, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-    print GPIO.input(i)
+GPIO.setup([27, 17], GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+print GPIO.input([27, 17])
