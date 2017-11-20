d4bfd3e526bf0be6a03289a032995aadf790dfa4
sharare90 <sharare.zehtabian@gmail.com>
2017-11-20 14:18:32 -0500

    read weight scale sensor 12

--- data_with_write.py
+++ data_with_write.py
@@ -1,8 +1,8 @@
 import RPi.GPIO as GPIO
+import HX711
 
 GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(27, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-print GPIO.input(27)
-print GPIO.input(17)
+GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+print GPIO.input(12)
+
