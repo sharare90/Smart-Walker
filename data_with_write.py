e70baaf60cbc24ed7ada259575c477c8c56615c7
sharare90 <sharare.zehtabian@gmail.com>
2017-11-20 15:19:13 -0500

    hx711

--- data_with_write.py
+++ data_with_write.py
@@ -1,156 +1,25 @@
-# import RPi.GPIO as GPIO
-# from hx711 import HX711
-#
-# GPIO.setmode(GPIO.BCM)
-#
-# GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-# print GPIO.input(12)
-#
-# hx0 = HX711()
-# hx1 = HX711()
-# hx2 = HX711()
-# hx3 = HX711()
-#
-# hx0.set_scale()
-# hx1.set_scale()
-# hx2.set_scale()
-# hx3.set_scale()
-#
-# hx0.tare()
-# hx1.tare()
-# hx2.tare()
-# hx3.tare()
-#
-# fl = hx0.get_units(10)
-# print fl
-
 import RPi.GPIO as GPIO
-import time
+from hx711 import HX711
 
-import sys
+GPIO.setmode(GPIO.BCM)
 
+GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+print GPIO.input(12)
 
-def createBoolList(size=8):
-    ret = []
-    for i in range(8):
-        ret.append(False)
-    return ret
+hx0 = HX711()
+hx1 = HX711()
+hx2 = HX711()
+hx3 = HX711()
 
+hx0.set_scale()
+hx1.set_scale()
+hx2.set_scale()
+hx3.set_scale()
 
-class HX711:
-    def __init__(self, dout, pd_sck, gain=128):
-        self.PD_SCK = pd_sck
-        self.DOUT = dout
+hx0.tare()
+hx1.tare()
+hx2.tare()
+hx3.tare()
 
-        GPIO.setmode(GPIO.BOARD)
-        # GPIO.setmode(GPIO.BCM)
-
-        self.GAIN = 0
-        self.OFFSET = 0
-        self.SCALE = 1
-        self.lastVal = 0
-
-        # GPIO.output(self.PD_SCK, True)
-        # GPIO.output(self.PD_SCK, False)
-
-        self.set_gain(gain);
-
-    def is_ready(self):
-        return GPIO.input(self.DOUT) == 0
-
-    def set_gain(self, gain):
-        if gain is 128:
-            self.GAIN = 1
-        elif gain is 64:
-            self.GAIN = 3
-        elif gain is 32:
-            self.GAIN = 2
-
-        GPIO.output(self.PD_SCK, False)
-        self.read()
-
-    def read(self):
-        while not self.is_ready():
-            # print("WAITING")
-            pass
-
-        dataBits = [createBoolList(), createBoolList(), createBoolList()]
-
-        for j in range(2, -1, -1):
-            for i in range(7, -1, -1):
-                GPIO.output(self.PD_SCK, True)
-                dataBits[j][i] = GPIO.input(self.DOUT)
-                GPIO.output(self.PD_SCK, False)
-
-        # set channel and gain factor for next reading
-        # for i in range(self.GAIN):
-        GPIO.output(self.PD_SCK, True)
-        GPIO.output(self.PD_SCK, False)
-
-        # check for all 1
-        if all(item == True for item in dataBits[0]):
-            return self.lastVal
-
-        bits = []
-        for i in range(2, -1, -1):
-            bits += dataBits[i]
-
-        self.lastVal = int(''.join(map(str, bits)), 2)
-        return self.lastVal
-
-        '''
-        data = [0,0,0]
-        for i in range(0,3):
-            #print(''.join(map(str, dataBits[i])))
-            data[i] = int(''.join(map(str, dataBits[i])), 2)
-            #print(data[i])
-        #data[2] ^= 0x80
-        return data[2] << 16 | data[1] << 8 | data[0]
-        '''
-
-    def read_average(self, times=3):
-        sum = 0
-        for i in range(times):
-            sum += self.read()
-
-        return sum / times
-
-    def get_value(self, times=3):
-        return self.read_average(times) - self.OFFSET
-
-    def get_units(self, times=3):
-        return self.get_value(times) / self.SCALE
-
-    def tare(self, times=15):
-        sum = self.read_average(times)
-        self.set_offset(sum)
-
-    def set_scale(self, scale):
-        self.SCALE = scale
-
-    def set_offset(self, offset):
-        self.OFFSET = offset
-
-    def power_down(self):
-        GPIO.output(self.PD_SCK, False)
-        GPIO.output(self.PD_SCK, True)
-
-    def power_up(self):
-        GPIO.output(self.PD_SCK, False)
-
-
-############# EXAMPLE
-hx = HX711(9, 11)
-hx.set_scale(7050)
-hx.tare()
-
-while True:
-    try:
-        val = hx.get_units(3)
-        if val > 100:
-            print("OH NO")
-            # hx.power_down()
-            # time.sleep(.001)
-            # hx.power_up()
-    except (KeyboardInterrupt, SystemExit):
-        sys.exit()
+fl = hx0.get_units(10)
+print fl
