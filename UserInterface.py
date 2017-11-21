70f967405e5def94bbab6a3b5b36bfe27b06ca93
sharare90 <sharare.zehtabian@gmail.com>
2017-11-21 12:54:56 -0500

    hx711_test_write values on the labels

--- UserInterface.py
+++ UserInterface.py
@@ -21,14 +21,67 @@ class SmartWalker(Widget):
     fr_text = StringProperty("")
     rl_text = StringProperty("")
     rr_text = StringProperty("")
-    hx711 = HX711()
+
+    @staticmethod
+    def get_4_weight_sensors():
+        hx0 = HX711(27, 17, 128)
+        hx1 = HX711(10, 22, 128)
+        hx2 = HX711(11, 9, 128)
+        hx3 = HX711(26, 13, 128)
+
+        hx0.set_reading_format("LSB", "MSB")
+        hx0.set_reference_unit(92)
+        hx0.reset()
+        hx0.tare()
+
+        hx1.set_reading_format("LSB", "MSB")
+        hx1.set_reference_unit(92)
+        hx1.reset()
+        hx1.tare()
+
+        hx2.set_reading_format("LSB", "MSB")
+        hx2.set_reference_unit(92)
+        hx2.reset()
+        hx2.tare()
+
+        hx3.set_reading_format("LSB", "MSB")
+        hx3.set_reference_unit(92)
+        hx3.reset()
+        hx3.tare()
+
+        while True:
+            try:
+                val0 = hx0.get_weight(5)
+                val1 = hx1.get_weight(5)
+                val2 = hx2.get_weight(5)
+                val3 = hx3.get_weight(5)
+                # print self.val0, ",", self.val1, ",", self.val2, ",", self.val3
+
+                hx0.power_down()
+                hx0.power_up()
+
+                hx1.power_down()
+                hx1.power_up()
+
+                hx2.power_down()
+                hx2.power_up()
+
+                hx3.power_down()
+                hx3.power_up()
+                time.sleep(0.5)
+                return val0, val1, val2, val3
+            except (KeyboardInterrupt, SystemExit):
+                hx0.cleanAndExit()
+                hx1.cleanAndExit()
+                hx2.cleanAndExit()
+                hx3.cleanAndExit()
 
     def update(self, *args):
         self.thisTime = str(time.asctime())
-        self.fl_text = str(self.hx711.val3)
-        self.rl_text = str(self.hx711.val2)
-        self.fr_text = str(self.hx711.val1)
-        self.rr_text = str(self.hx711.val0)
+        self.rr_text = str(self.get_4_weight_sensors()[0])
+        self.fr_text = str(self.get_4_weight_sensors()[1])
+        self.rl_text = str(self.get_4_weight_sensors()[2])
+        self.fl_text = str(self.get_4_weight_sensors()[3])
 
     def change_color(self, leg):
         if leg == self.front_left_leg:
--- data_with_write.py
+++ data_with_write.py
@@ -10,10 +10,10 @@ class HX711:
         self.PD_SCK = pd_sck
         self.DOUT = dout
 
-        self.val0 = 0  # rr
-        self.val1 = 0  # fr
-        self.val2 = 0  # rl
-        self.val3 = 0  # fl
+        # self.val0 = 0  # rr
+        # self.val1 = 0  # fr
+        # self.val2 = 0  # rl
+        # self.val3 = 0  # fl
 
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.PD_SCK, GPIO.OUT)
@@ -188,56 +188,5 @@ class HX711:
         print "Bye!"
         sys.exit()
 
-    def get_values_to_print(self):
 
-        hx0 = HX711(27, 17, 128)
-        hx1 = HX711(10, 22, 128)
-        hx2 = HX711(11, 9, 128)
-        hx3 = HX711(26, 13, 128)
 
-        hx0.set_reading_format("LSB", "MSB")
-        hx0.set_reference_unit(92)
-        hx0.reset()
-        hx0.tare()
-
-        hx1.set_reading_format("LSB", "MSB")
-        hx1.set_reference_unit(92)
-        hx1.reset()
-        hx1.tare()
-
-        hx2.set_reading_format("LSB", "MSB")
-        hx2.set_reference_unit(92)
-        hx2.reset()
-        hx2.tare()
-
-        hx3.set_reading_format("LSB", "MSB")
-        hx3.set_reference_unit(92)
-        hx3.reset()
-        hx3.tare()
-
-        while True:
-            try:
-                self.val0 = hx0.get_weight(5)
-                self.val1 = hx1.get_weight(5)
-                self.val2 = hx2.get_weight(5)
-                self.val3 = hx3.get_weight(5)
-                print self.val0, ",", self.val1, ",", self.val2, ",", self.val3
-
-                hx0.power_down()
-                hx0.power_up()
-
-                hx1.power_down()
-                hx1.power_up()
-
-                hx2.power_down()
-                hx2.power_up()
-
-                hx3.power_down()
-                hx3.power_up()
-                time.sleep(0.5)
-
-            except (KeyboardInterrupt, SystemExit):
-                hx0.cleanAndExit()
-                hx1.cleanAndExit()
-                hx2.cleanAndExit()
-                hx3.cleanAndExit()
