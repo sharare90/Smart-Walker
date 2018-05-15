e0c9b557805ab0bfa26a4695d6a2848fa08d3492
Christopher Feltner <chris.feltner@knights.ucf.edu>
2018-05-15 12:56:09 -0400

    removed ServerLogger

--- logger.py
+++ logger.py
@@ -12,10 +12,10 @@ class Logger(object):
         for char in ('.', ':', ' '):
             file_name = file_name.replace(char, '-')
         file_name += '.txt'
-        self.upload_data = True
+        self._upload_data = True
         self._current_data = ''
-
-
+        response = requests.post('http://10.173.215.128:8000/create_file')
+        self._server_file_name = json.loads(response.content)['file_name']
         self.file = open('logs/' + file_name, 'w')
         self.write_header()
 
@@ -59,7 +59,7 @@ class Logger(object):
             try:
                 if(urlopen('http://10.173.215.128:8000/').getcode() == 200):
                     return True
-                else
+                else:
                     return False
             except Exception:
                 return False
@@ -67,40 +67,47 @@ class Logger(object):
     def set_upload_data(self, upload):
         self.upload_data = upload
 
+    def upload_data(self):
+        requests.post('http://10.173.215.128:8000/add_line', data={
+            'line': self._current_data,
+            'file_name': self._server_file_name,
+        })
+
+
 if __name__ == '__main__':
     Logger()
 
 
-class ServerLogger(object):
+# class ServerLogger(object):
 
-    def __init__(self):
-        response = requests.post('http://10.173.215.128:8000/create_file')
-        self.file_name = json.loads(response.content)['file_name']
-        self.line = ''
+#     def __init__(self):
+#         response = requests.post('http://10.173.215.128:8000/create_file')
+#         self.file_name = json.loads(response.content)['file_name']
+#         self.line = ''
 
-    def update_sensors(self, fr, fl, rr, rl):
-        self.line = ('{time}, {fr}, {fl}, {rr}, {rl}, '.format(
-            time=datetime.now(),
-            fr=fr,
-            fl=fl,
-            rr=rr,
-            rl=rl,
-        ))
+#     def update_sensors(self, fr, fl, rr, rl):
+#         self.line = ('{time}, {fr}, {fl}, {rr}, {rl}, '.format(
+#             time=datetime.now(),
+#             fr=fr,
+#             fl=fl,
+#             rr=rr,
+#             rl=rl,
+#         ))
 
-    def update_gyro(self, head, roll, pitch, sys, gyro, acc, mag):
-        self.line += ('{head}, {roll}, {pitch}, {sys}, {gyro}, {acc}, {mag}, '.format(
-            head=head,
-            roll=roll,
-            pitch=pitch,
-            sys=sys,
-            gyro=gyro,
-            acc=acc,
-            mag=mag, 
-        ))
+#     def update_gyro(self, head, roll, pitch, sys, gyro, acc, mag):
+#         self.line += ('{head}, {roll}, {pitch}, {sys}, {gyro}, {acc}, {mag}, '.format(
+#             head=head,
+#             roll=roll,
+#             pitch=pitch,
+#             sys=sys,
+#             gyro=gyro,
+#             acc=acc,
+#             mag=mag, 
+#         ))
 
-    def update_proximity(self, proximity):
-        self.line += str(proximity)
-        requests.post('http://10.173.215.128:8000/add_line', data={
-            'line': self.line,
-            'file_name': self.file_name,
-        })
+#     def update_proximity(self, proximity):
+#         self.line += str(proximity)
+#         requests.post('http://10.173.215.128:8000/add_line', data={
+#             'line': self.line,
+#             'file_name': self.file_name,
+#         })
