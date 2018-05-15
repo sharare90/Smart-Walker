f11bf506a2987b39f9d5746200ec2d296cf5b3d6
Christopher Feltner <chris.feltner@knights.ucf.edu>
2018-05-15 14:36:45 -0400

    set data to None

--- logger.py
+++ logger.py
@@ -71,7 +71,7 @@ class Logger(object):
         self.file.write(self._current_data)
         self.file.write('\n')
         self.file.flush()
-        self.upload_data(self)
+        self.upload_data(self, None)
 
     # is_server_available(self)
     # tests the connection to the server
