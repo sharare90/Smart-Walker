efd025a060566876b8570736e8ca134ccff14089
Christopher Feltner <chris.feltner@knights.ucf.edu>
2018-05-23 12:06:49 -0400

    WeightSensor subclass

--- WeightSensor.py
+++ WeightSensor.py
@@ -6,7 +6,7 @@ class WeightSensor(HX711):
         super(WeightSensor, self).__init__(dout, pd_sck)
 
     def initialize_weight_sensor(self):
-        sensor.set_reading_format("LSB", "MSB")
-        sensor.set_reference_unit(92)
-        sensor.reset()
-        sensor.tare()
+        self.set_reading_format("LSB", "MSB")
+        self.set_reference_unit(92)
+        self.reset()
+        self.tare()
