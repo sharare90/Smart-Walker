232d5059884b3f27a0301e4e88e452555994378d
Christopher Feltner <chris.feltner@knights.ucf.edu>
2018-05-23 11:10:05 -0400

    upload frequency

--- settings.py
+++ settings.py
@@ -5,3 +5,4 @@ CREATE_FILE_URL = SERVER_URL+'create_file'
 LOG_FILE_DIRECTORY = 'logs/'
 LOG_IMAGE_FILE_DIRECTORY = 'images_logs/'
 FILE_HEADER = 'time, fr, fl, rr, rl, head, roll, pitch, sys, gyro, acc, mag, proximity'
+UPLOAD_FREQUENCY = 100
