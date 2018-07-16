84acd4501beab176cfe012c67daeeb2c31438c61
Sharare <sharare.zethabian@gmail.com>
2018-07-16 15:03:59 -0400

    make image upload another thread.

---
+++ capture_photo.py
@@ -0,0 +1,33 @@
+import json
+import pygame
+
+import requests
+
+import datetime
+from settings import CREATE_DIRECTORY_URL, LOG_IMAGE_FILE_DIRECTORY, URL_IMAGES
+
+
+def create_image_directory():
+    response = requests.post(CREATE_DIRECTORY_URL)
+    return json.loads(response.content)['directory_name']
+
+
+def capture_photos(cam, server_image_directory_name):
+    img = cam.get_image()
+    image_file_name = LOG_IMAGE_FILE_DIRECTORY + 'pic.jpg'
+    pygame.image.save(img, image_file_name)
+    with open(image_file_name) as f:
+        requests.post(URL_IMAGES, files={'file': (image_file_name, f)},
+                      data={'directory_name': server_image_directory_name})
+
+
+def start():
+    server_image_directory_name = create_image_directory()
+    pygame.camera.init()
+    cam = pygame.camera.Camera(pygame.camera.list_cameras()[0])
+    cam.start()
+    for i in range(5):
+        capture_photos(cam, server_image_directory_name)
+
+
+start()
